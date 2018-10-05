f381144e47e0819274a0218e1d6f2b61fb5734aa
gabriel alcantara rocha <43858950+gabrielalcantarar@users.noreply.github.com>
2018-10-05 19:33:17 -0300

    Update main.c

--- 2018.1-IAC/AB2.1-TP/src/main.c
+++ 2018.1-IAC/AB2.1-TP/src/main.c
@@ -1,33 +1,75 @@
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/wait.h> 
-#include <stdio.h> 
+#include <unistd.h>
+#include<time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 int main (int argc, char *argv[], char *envp[]) {
+int pid ; /* Process ID */
+char mem_usage[50];
+char cpu_usage[50];
+char kill[50];
+clock_t t1,t0;
+double td = 0;
+pid = fork () ; /* Process reaplication */
+sprintf(cpu_usage,"%s%d%s","ps -p ", pid, " -o %cpu | grep -v %CPU");
+sprintf(mem_usage, "%s%d%s", "pmap -x ", pid," | grep total | awk '{print $3}'");
+sprintf(kill, "%s%d", "kill -9 ", pid);
 
-int pid ; /* identificador de processo */
-
-pid = fork () ; /* replicacção do processo */
-
-if ( pid < 0 ) { /* se o fork não funcionou */
-	perror ("Erro: ") ;
-	exit (-1) ; /* encerra o processo com código de erro -1 */ 
+if ( pid < 0 ) { /*if fork do not work*/
+  perror ("Erro: ") ;
+  exit (-1) ; /* Ends process with error: -1 */
 }
-else if( pid > 0 ) /* se sou o processo pai*/ 
+else if( pid > 0 ) /* If i'm parent process*/
 {
-	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
-	//TODO após 10 segundos de execução, mate o proceso filho
+  for(int i=0;i<10;i++)
+  {
+    system("clear");
+    if(i == 0 || i == 3 || i == 6 || i == 9)
+    {
+    printf("Processing.\n");
+    }
+    else if(i == 1 || i == 4 || i == 7)
+    {
+      printf("Processing..\n");
+    }
+    else if(i == 2 || i == 5 || i == 8)
+    {
+      printf("Processing...\n");
+    }
+    printf("Process ID: %d\n", pid);
+    printf("%ds\n", i+1);
+    printf("================\n");
+    printf("CPU(%%)\n");
+    system(cpu_usage);
+    printf("MEM(kB)\n");
+    system(mem_usage);
+    sleep(1);
+  }
+  system(kill);
 }
-else /* senão, sou o processo filho (pid == 0) */ 
+else /* else, i'm a child process (pid == 0) */
 {
-	//TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória
+  if(strcmp(argv[1],"ucp")==0) /* if argv[1] = ucp -> Execute code using intese cpu*/
+  {
+    for(;;){}
+  }
+  if(strcmp(argv[1],"ucp-mem")==0) /* if argv[1] = ucp-mem -> Execute code using intese cpu and memory */
+  {
+    t0 = clock();
+    for (;;) {
+      t1 = clock();
+     td = (double)(t1 - t0) / CLOCKS_PER_SEC;
+      if (td <= 10.0000){
+          malloc(15*sizeof(int));
+        }
+}
+  }
 
 }
-perror ("Erro: ") ; /* execve nãoo funcionou */
+perror ("Erro: ") ; /* if do not work */
 
 printf ("Tchau !\n") ;
-exit(0) ; /* encerra o processo com sucesso (código 0) */ 
+exit(0) ; /* Ends process with success (código 0) */
 
 }
